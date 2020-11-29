2d41c827c0658a0ba9fb04c7d69b18308474823f
Samir M. Eltabal <samir.m.eltabal@gmail.com>
2020-11-29 18:08:23 +0200

    add dist to tracking

---
+++ dist/teacher.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!***************************!*\
+  !*** ./src/js/teacher.js ***!
+  \***************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+
+/******/ })()
+;
